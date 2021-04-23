23b9f30e82108a938009e96621b7bc88a6234a76
Samantha Darris <darriss@remote1.cs.allegheny.edu>
2021-04-23 00:16:21 -0400

    Final Calculations

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Be sure comment out each the Bar Graph section when running the line graphs and vice versa!
--- comp.py
+++ comp.py
@@ -10,33 +10,34 @@ from scipy import stats
 from IPython.display import display, HTML
 plt.style.use('bmh')
 from pandas.plotting import scatter_matrix
-compare = pd.read_csv("mardata.csv")
+compare = pd.read_csv("newmardata.csv")
 meeting = pd.read_csv("meetingstats.csv")
-print(meeting.head())  # Show first 5 rows)
-#print(compare.columns)
 
 #This part is defining all the variables for each indicator in education data
 x = compare['Years']
-y1 = compare['Lower secondary completion rate, male (% of relevant age group)']
-y2 = compare['Lower secondary completion rate, female (% of relevant age group)']
-tvx = compare['% married M [15-19]']
-tvy = compare['% married F [15-19]']
-#This part is defining all the variables for each indicator in meeting in marriage data
-xm = meeting['Circumstance of Meeting']
-casa = meeting['Casablanca']
-tan = meeting['Tangier']
-'''
-#Bar graph
-plt.title('Circumstance of Meeting')
-plt.xlabel('How they Met', fontsize=4)
-plt.ylabel('Percentage', fontsize=16)
-plt.bar(xm, casa)
-plt.bar(xm, tan)
-'''
-labels = ['Family Meeting', 'Blind Engagement', 'Place of Study', 'Workplace']
-casa_means = [13.9, 5.9, 6.6, 16]
-tan_means = [23.2, 1.2, 8.3, 7.5]
-'''
+y1 = compare['School enrollment, tertiary, male (% gross)']
+x1 = compare['School enrollment, tertiary, female (% gross)']
+y2 = compare['Lower secondary completion rate, male (% of relevant age group)']
+x2 = compare['Lower secondary completion rate, female (% of relevant age group)']
+md = compare['Number of maternal deaths']
+sepsis = compare['Death by sepsis and other infections for children under 5 %']
+anomaly = compare['Death by congenital anomalies under 5 years old %']
+laborx = compare['Labor force participation rate, female (% of female population ages 15+) (modeled ILO estimate)']
+labory = compare['Labor force participation rate for ages 15-24, male (%) (national estimate)']
+tvy = compare['% married M [15-19]']
+tvx = compare['% married F [15-19]']
+tvy1 = compare['% married M [20-24]']
+tvx1 = compare['% married F [20-24]']
+tvy2 = compare['% married M [25-29]']
+tvx2 = compare['% married F [25-29]']
+space =' ';
+
+#Bar Graph section
+
+labels = ['Family Meeting', 'Relationship with Neighbors', 'Blind Engagement', 'Public Place', 'School', 'Workplace']
+casa_means = [13.9, 17.2, 5.9, 22.6, 6.6, 16]
+tan_means = [23.2, 5.8, 1.2, 22.2, 8.3, 7.5]
+
 x = np.arange(len(labels))  # the label locations
 width = 0.35  # the width of the bars
 
@@ -45,39 +46,64 @@ rects1 = ax.bar(x - width/2, casa_means, width, label='Casablanca')
 rects2 = ax.bar(x + width/2, tan_means, width, label='Tangier')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_xlabel('Meeting Place')
 ax.set_ylabel('Percentage (%)')
 ax.set_title('Circumstance of Meeting in 2011')
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.legend()
-
 fig.tight_layout()
-
 plt.show()
-'''
-#Line graph
+
+#Line graph section
 #This labels the axix's and the graph
-plt.title('Compare')
-plt.xlabel('2010', fontsize=10)
-plt.ylabel('Indicators', fontsize=10)
+plt.title('Congenital Anomaly Death Rate of Children Ages (0-4) in Morocco')
+plt.xlabel('Years', fontsize=10)
+plt.ylabel('Percentage %', fontsize=10)
 #These are each of the plots llalong the x axis
-plt.plot(x, y1, marker='o', label='male completion')
-plt.plot(x, y2, marker='o', label='female completion')
-plt.plot(x, tvx, marker='o', label='male marriage rate')
-plt.plot(x, tvy, marker='o', label='female marriage rate')
-#This shows what each line represents
-#pd.tools.plotting.scatter_matrix(data.loc[:, y2:tvy], diagonal="kde")
-
-
-pd.plotting.scatter_matrix(compare.loc[:, "School enrollment, tertiary, female (% gross)":"% married F [15-19]"], diagonal="kde")
-plt.tight_layout()
+plt.plot(x, x1, marker='o', label='female college enrollment rate')
+plt.plot(x, y1, marker='o', label='male college enrollment rate')
+plt.plot(x, tvy1, marker='o', label='male marriage rate 20-24')
+plt.plot(x, tvx1, marker='o', label='female marriage rate 20-24')
+plt.plot(x, tvy2, marker='o', label='male marriage rate')
+plt.plot(x, tvx2, marker='o', label='female marriage rate')
+plt.plot(x, laborx, marker='o', label='female labor participation rate (ages 15+)')
+plt.plot(x, labory, marker='o', label='male labor participation rate (ages 15-24)')
+plt.plot(x, anomaly, marker='o')
+plt.plot(x, y2, marker='o', label='male middle completion school')
+plt.plot(x, x2, marker='o', label='female middle completion school')
+plt.plot(x, tvy, marker='o', label='male marriage rate (15-19)')
+plt.plot(x, tvx, marker='o', label='female marriage rate (15-19)')
+plt.legend(loc='best')
 plt.show()
 
-print('y1 mean:', np.mean(y1))
-print('y1 standard deviation:', np.std(y1))
-
-print('covariance y1 and tvx', np.cov(y1, tvx))
-
-# use pearson correlation if there is a linear relationship between variables
-# use spearman correlation if the relationship is non-linear
-print('correlation y1 and tvx', scipy.stats.spearmanr(y1, tvx))
+#These are the mean, standard deviation, covariance,and correlation calculations
+print('Mean and Standard Deviation of Variables')
+print(space);
+print('male college enrollment rate mean:', np.mean(y1))
+print('male college enrollment rate standard deviation:', np.std(y1))
+print('male marriage rate mean (20-24):', np.mean(tvy1))
+print('male marriage rate standard deviation (20-24):', np.std(tvy1))
+print('male marriage rate mean (25-29):', np.mean(tvy2))
+print('male marriage rate standard deviation (25-29):', np.std(tvy2))
+print(space);
+print('female college enrollment rate mean:', np.mean(x1))
+print('female college enrollment rate standard deviation:', np.std(x1))
+print('female marriage rate mean (20-24):', np.mean(tvx1))
+print('female marriage rate standard deviation (20-24):', np.std(tvx1))
+print('female marriage rate mean (25-29):', np.mean(tvx2))
+print('female marriage rate standard deviation (25-29):', np.std(tvx2))
+print('female labor participation rate mean (15+):', np.mean(laborx))
+print('female labor participation rate standard deviation (15+):', np.std(laborx))
+print(space);
+print('sepsis death rate mean (0-4):', np.mean(sepsis))
+print('sepsis death rate standard deviation (0-4):', np.std(sepsis))
+print('congenital anomaly death rate mean (0-4):', np.mean(anomaly))
+print('congenital anomaly death rate standard deviation (0-4):', np.std(anomaly))
+print('maternal death rate mean:', np.mean(md))
+print('maternal death rate standard deviation:', np.std(md))
+print(space);
+print('Covariance and Correlation of Labor and Education')
+print(space)
+print('female college enrollment and labor participation rate covariance', np.cov(x1, laborx))
+print('Pearsons correlation of female college enrollment and labor participation rate', scipy.stats.pearsonr(x1, laborx))
